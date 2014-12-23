eaf6709aa8f44ada03031937efdcac5d15abeca6
Jeff Doolittle <jeffdoolittle@gmail.com>
2014-12-23 06:49:02 -0800

    added date property to product

--- src/Driven.SampleDomain/Product.cs
+++ src/Driven.SampleDomain/Product.cs
@@ -1,39 +1,44 @@
-﻿namespace Driven.SampleDomain
+﻿using System;
+
+namespace Driven.SampleDomain
 {
     public class Product : Aggregate
     {
         private readonly TenantId _tenantId;
         private readonly ProductId _productId;
         private readonly ProductName _productName;
+        private readonly DateTimeOffset _createdAt;
 
         protected Product() { }
 
-        private Product(TenantId tenantId, ProductId productId, ProductName productName)
+        private Product(TenantId tenantId, ProductId productId, ProductName productName, DateTimeOffset createdAt)
         {
             _tenantId = tenantId;
             _productId = productId;
             _productName = productName;
+            _createdAt = createdAt;
         }
 
         public static Product Create(TenantId tenantId, ProductId productId, ProductName productName)
         {
-            var product = new Product(tenantId, productId, productName);
-            product.ApplyProductCreatedEvent(tenantId.Id(), productId.Id(), productName.Name);
+            var createdAt = DateTimeOffset.Now;
+            var product = new Product(tenantId, productId, productName, createdAt);
+            product.ApplyProductCreatedEvent(tenantId.Id(), productId.Id(), productName.Name, createdAt);
             return product;
         }
 
         public Product ChangeName(ProductName newName)
         {
             var currentName = _productName.Name;
-            var updated = new Product(_tenantId, _productId, newName);
+            var updated = new Product(_tenantId, _productId, newName, _createdAt);
             updated.SetIdentity(GetIdentity());
             updated.ApplyProductNameChangedEvent(_tenantId.Id(), _productId.Id(), currentName, newName.Name);
             return updated;
         }
 
-        private void ApplyProductCreatedEvent(string tenantId, string productId, string productName)
+        private void ApplyProductCreatedEvent(string tenantId, string productId, string productName, DateTimeOffset createdAt)
         {
-            var e = new ProductCreatedEvent(tenantId, productId, productName);
+            var e = new ProductCreatedEvent(tenantId, productId, productName, createdAt);
             ApplyEvent(e);
         }
 
@@ -46,24 +51,26 @@
 
     public class ProductCreatedEvent : Identifiable
     {
-        private string _tenantId;
-        private string _productId;
-        private string _productName;
+        private readonly string _tenantId;
+        private readonly string _productId;
+        private readonly string _productName;
+        private readonly DateTimeOffset _createdAt;
 
         protected ProductCreatedEvent() { }
 
-        public ProductCreatedEvent(string tenantId, string productId, string productName)
+        public ProductCreatedEvent(string tenantId, string productId, string productName, DateTimeOffset createdAt)
         {
             _tenantId = tenantId;
             _productId = productId;
             _productName = productName;
+            _createdAt = createdAt;
         }
     }
 
     public class ProductNameChangedEvent : Identifiable
     {
-        private string _tenantId;
-        private string _productId;
+        private readonly string _tenantId;
+        private readonly string _productId;
         private readonly string _originalProductName;
         private readonly string _newProductName;
 
